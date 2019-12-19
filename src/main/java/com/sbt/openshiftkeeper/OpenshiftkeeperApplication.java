f7339b68a13cbfff0e12c66b8d5d8c18c1b9a6e1
Segstas <segstas@gmail.com>
2019-12-19 23:58:55 +0300

    Init

---
+++ src/main/java/com/sbt/openshiftkeeper/OpenshiftkeeperApplication.java
@@ -0,0 +1,13 @@
+package com.sbt.openshiftkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftkeeperApplication.class, args);
+	}
+
+}
